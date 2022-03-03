2bc745a735a2f95fd0f9b9b96c177217c814e811
Hyy <1763532961@qq.com>
2022-03-03 22:17:46 +0800

    更改前后端交互方式为ajax+json格式

--- src/main/java/com/sxau/controller/EmployeeController.java
+++ src/main/java/com/sxau/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.sxau.controller;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.sxau.bean.Employee;
+import com.sxau.bean.Msg;
 import com.sxau.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -18,15 +20,14 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
+    @ResponseBody
     @RequestMapping("/emps")
-    public String getEmps(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model){
+    public Msg getEmpsWithJson(@RequestParam(value = "pn", defaultValue = "1") Integer pn){
         //引入Pagehelper分页插件，传入页码，每页大小
         PageHelper.startPage(pn,5);
         //startpage之后的查询即为分页查询
         List<Employee> employeeList=employeeService.getAll();
         //pageInfo封装了详细的分页信息和查询数据，传入导航页数，并将其交给页面
-        PageInfo pageInfo =new PageInfo(employeeList,5);
-        model.addAttribute("pageinfo",pageInfo);
-        return "list";
+        return Msg.success().add("pageInfo", new PageInfo(employeeList,5));
     }
 }
--- web/index.jsp
+++
@@ -1,9 +0,0 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: hyy
-  Date: 2022/2/27
-  Time: 21:23
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<jsp:forward page="${pageContext.request.contextPath}/emps"/>
